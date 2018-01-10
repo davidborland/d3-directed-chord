e3de47471ebdc2c44df674cee6c21b99873eb360
David Borland <borland@renci.org>
2018-01-10 14:18:12 -0500

    Static visualization working

--- vis/d3.directedChord.js
+++ vis/d3.directedChord.js
@@ -26,7 +26,11 @@
         // Data
         data = [],
         nodes = [],
-        matrix = [],
+        chords = [],
+
+        // Layout
+        nodeHeight = 40,
+        zoneHeight = 10,
 
         // Start with empty selections
         svg = d3.select();
@@ -49,10 +53,11 @@
             .attr("class", "directedChord");
 
         g = svgEnter.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("transform", "translate(" + (margin.left + innerWidth() / 2) + "," +
+                                              (margin.top + innerHeight() / 2) + ")");
 
         // Groups for layout
-        var groups = ["chords", "zones", "nodes"];
+        var groups = ["chords", "nodes", "zones"];
 
         g.selectAll("g")
             .data(groups)
@@ -71,7 +76,7 @@
         return d[""];
       });
 
-      matrix = [];
+      var matrix = [];
 
       data.forEach(function(r, i) {
         // Outgoing and incoming per row
@@ -95,6 +100,7 @@
         matrix.push(incoming);
       });
 
+      // XXX: Put in check for all-zero row or column?
 /*
       console.log(data.map(function(d) {
         return nodes.map(function(n) {
@@ -102,7 +108,11 @@
         });
       }));
       console.log(matrix);
-*/      
+*/
+      var chordLayout = d3.chord()
+      .padAngle(0.04);
+
+      chords = chordLayout(matrix);
     }
 
     function draw() {
@@ -110,18 +120,181 @@
       svg .attr("width", width)
           .attr("height", height);
 
+      // Three raddii defining zones and nodes
+      var size = Math.min(innerWidth(), innerHeight()),
+          outerRadius = size / 2,
+          innerRadius = outerRadius - nodeHeight;
+
       drawChords();
       drawZones();
       drawNodes();
 
       function drawChords() {
+        var ribbon = d3.ribbon()
+            .radius(innerRadius);
+
+        // Bind chord data
+        var chord = svg.select(".chords").selectAll(".chord")
+            .data(chords);
+
+        // Enter
+        var chordEnter = chord.enter().append("path")
+            .attr("class", "chord")
+            .style("fill", "#666")
+            .style("fill-opacity", 0.5);
+
+        chordEnter.append("title")
+            .text(function(d) {
+              return d.source.index + "->" + d.target.index + ": " + d.source.value + ", " + d.target.value;
+            });
+
+        // Enter + update
+        chordEnter.merge(chord)
+            .attr("d", ribbon);
+
+        // Exit
+        chord.exit().remove();
       }
 
       function drawZones() {
+        var arc = d3.arc()
+            .innerRadius(innerRadius - zoneHeight / 2)
+            .outerRadius(innerRadius + zoneHeight / 2)
+            .cornerRadius(5);
+
+        // Bind chord group data
+        var zone = svg.select(".zones").selectAll(".zone")
+            .data(chords.groups);
+
+        // Enter
+        var zoneEnter = zone.enter().append("g")
+            .attr("class", "zone");
+
+        zoneEnter.append("path")
+            .style("fill", function(d, i) {
+              return i % 2 === 0 ? "#b2182b" : "#2166ac";
+            });
+
+        // Enter + update
+        var zoneUpdate = zoneEnter.merge(zone);
+
+        zoneUpdate.select("path")
+            .attr("d", arc);
+
+        // Exit
+        zone.exit().remove();
       }
 
       function drawNodes() {
+        // Combine outgoing and incoming zones
+        var nodeData = nodes.map(function(d, i) {
+          // Two groups per node
+          var g1 = chords.groups[i * 2],
+              g2 = chords.groups[i * 2 + 1];
 
+          return {
+            name: d,
+            index: i,
+            startAngle: g1.startAngle - 0.01,
+            endAngle: g2.endAngle + 0.01,
+            value: g1.value + g2.value
+          }
+        });
+
+        var arc = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius)
+            .cornerRadius(5);
+
+        // Bind node data
+        var node = svg.select(".nodes").selectAll(".node")
+            .data(nodeData);
+
+        // Enter
+        var nodeEnter = node.enter().append("path")
+            .attr("class", "node")
+            .style("fill", "#ddd")
+            .style("stroke", "#999")
+            .style("stroke-width", 2);
+
+        // Enter + update
+        nodeEnter.merge(node)
+            .attr("d", arc);
+
+        // Exit
+        node.exit().remove();
+
+        // Create a path for labels
+        var r = (innerRadius + outerRadius) / 2,
+            c = 2 * Math.PI * r;
+
+        var circleArc1 = d3.arc()
+            .innerRadius(r)
+            .outerRadius(r)
+            .startAngle(0)
+            .endAngle(2 * Math.PI);
+
+        var circleArc2 = d3.arc()
+            .innerRadius(r)
+            .outerRadius(r)
+            .startAngle(0)
+            .endAngle(-2 * Math.PI);
+
+        var circles = svg.select(".nodes").selectAll(".labelCircle")
+            .data([circleArc1, circleArc2])
+          .enter().append("path")
+            .attr("class", "labelCircle")
+            .attr("id", function(d, i) { return circleId(i); })
+            .attr("d", function(d) { return d(); })
+            .style("fill", "none")
+            .style("visibility", "hidden");
+
+        // Bind node data for labels
+        var label = svg.select(".nodes").selectAll("g")
+            .data(nodeData);
+
+        // Enter
+        var labelEnter = label.enter().append("text")
+            .attr("class", "nodeLabel")
+            .style("text-anchor", "middle")
+            .style("dominant-baseline", "middle")
+            .style("font-family", "sans-serif");
+
+        labelEnter.append("textPath")
+            .attr("xlink:href", function(d) {
+              var i = flip(d) ? 1 : 0;
+
+              return "#" + circleId(i);
+            });
+
+        // Enter + update
+        labelEnter.merge(label)
+            .attr("dx", dx)
+          .select("textPath")
+            .text(function(d) { return d.name; });
+
+        // Exit
+        node.exit().remove();
+
+        function dx(d) {
+          var x = c * mid(d) / (2 * Math.PI);
+
+          return flip(d) ? c - x : x;
+        }
+
+        function circleId(i) {
+          return "labelCircle" + i;
+        }
+
+        function mid(d) {
+          return (d.startAngle + d.endAngle) / 2;
+        }
+
+        function flip(d) {
+          var a = mid(d);
+
+          return a > Math.PI / 2 && a < 3 * Math.PI / 2 ? 1 : 0;
+        }
       }
     }
 
